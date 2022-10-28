52c0654e7ce412aef22d55ea9b61291cc1b13a96
Vasiliy Rusanov <rvk1986@mail.ru>
2022-10-28 15:18:35 +0300

    Выполнил задания

--- js/script.js
+++ js/script.js
@@ -4,39 +4,50 @@ const todoControl = document.querySelector('.todo-control');
 const headerInput = document.querySelector('.header-input');
 const todoList = document.querySelector('.todo-list');
 const todoCompleted = document.querySelector('.todo-completed');
+let toDoData = [];
 
-const toDoData = [];
 
+
+if (localStorage.todo) {
+    toDoData = JSON.parse(localStorage.getItem('todo'));
+}
 const render = function () {
     todoList.innerHTML = '';
     todoCompleted.innerHTML = '';
-    toDoData.forEach(function(item){
+    toDoData.forEach(function (item, index) {
         const li = document.createElement('li');
         li.classList.add('todo-item');
 
-        li.innerHTML = '<span class="text-todo">' + item.text + '</span>' + 
-        '<div class="todo-buttons">' + 
-        '<button class="todo-remove"></button>' + 
-        '<button class="todo-complete"></button>' + 
-        '</div>';
+        li.innerHTML = '<span class="text-todo">' + item.text + '</span>' +
+            '<div class="todo-buttons">' +
+            '<button class="todo-remove"></button>' +
+            '<button class="todo-complete"></button>' +
+            '</div>';
 
-        if(item.completed) {
+        if (item.completed) {
             todoCompleted.append(li);
+            localStorage.setItem('todo', [JSON.stringify(toDoData)]);
         } else {
             todoList.append(li);
+            localStorage.setItem('todo', [JSON.stringify(toDoData)]);
         }
-        
-        li.querySelector('.todo-complete').addEventListener('click', function() {
+
+        li.querySelector('.todo-complete').addEventListener('click', function () {
             item.completed = !item.completed;
             render();
         });
-        li.querySelector('.todo-remove').addEventListener('click', function() {
+        li.querySelector('.todo-remove').addEventListener('click', function () {
             li.remove();
-            toDoData.pop();
-            
+
+            toDoData.splice(index, 1);
+            render();
+            console.log(toDoData);
+            localStorage.setItem('todo', [JSON.stringify(toDoData)]);
+
         });
 
     });
+
 };
 
 
@@ -51,9 +62,12 @@ todoControl.addEventListener('submit', function (event) {
         };
 
         toDoData.push(newToDo);
+
+        localStorage.setItem('todo', [JSON.stringify(toDoData)]);
         headerInput.value = '';
 
         render();
     }
 
 });
+render();
